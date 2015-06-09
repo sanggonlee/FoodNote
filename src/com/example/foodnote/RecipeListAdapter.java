83f3f53f4bca78cb3ccaf3322c5fd10e9ba1af7b
sanggonlee <lee.sg16@gmail.com>
2015-06-09 00:13:50 -0400

    initial commit

---
+++ src/com/example/foodnote/RecipeListAdapter.java
@@ -0,0 +1,108 @@
+package com.example.foodnote;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class RecipeListAdapter extends BaseAdapter {
+	private final List<RecipeItem> mItems = new ArrayList<RecipeItem>();
+	private final Context mContext;
+
+	private static final String TAG = "RecipeListAdapter";
+
+	public RecipeListAdapter(Context context) {
+		mContext = context;
+	}
+
+	// Add a RecipeItem to the adapter
+	// Notify observers that the data set has changed
+	public void add(RecipeItem item) {
+		mItems.add(item);
+		notifyDataSetChanged();
+	}
+
+	// Clears the list adapter of all items.
+	public void clear() {
+		mItems.clear();
+		notifyDataSetChanged();
+	}
+
+	// Returns the number of RecipeItems
+	@Override
+	public int getCount() {
+		return mItems.size();
+	}
+
+	// Retrieve the number of RecipeItems
+	@Override
+	public Object getItem(int pos) {
+		return mItems.get(pos);
+	}
+
+	// Get the ID for the RecipeItem
+	// In this case it's just the position
+	@Override
+	public long getItemId(int pos) {
+		return pos;
+	}
+
+	// Create a View for the RecipeItem at specified position
+	// Remember to check whether convertView holds an already allocated View
+	// before created a new View.
+	// TODO: Consider using the ViewHolder pattern to make scrolling more efficient
+	// See: http://developer.android.com/training/improving-layouts/smooth-scrolling.html
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		final RecipeItem recipeItem = mItems.get(position);
+
+		// Inflate the View for this RecipeItem from recipe_item.xml
+		RelativeLayout itemLayout = convertView != null ? (RelativeLayout) convertView :
+					(RelativeLayout) LayoutInflater.from(mContext)
+				.inflate(R.layout.recipe_item, parent, false);
+
+		// Display Title in TextView
+		final TextView titleView = (TextView)itemLayout.getChildAt(0);
+		titleView.setText(recipeItem.getTitle());
+		
+		/*
+		// Set up Status CheckBox
+		final CheckBox statusView = (CheckBox)itemLayout.getChildAt(2);
+		statusView.setChecked(recipeItem.getStatus() == RecipeItem.Status.DONE);
+
+		statusView.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				Log.i(TAG, "Entered onCheckedChanged()");
+
+				// TODO - set up an OnCheckedChangeListener, which
+				// is called when the user toggles the status checkbox
+				buttonView.setChecked(isChecked);
+			}
+		});
+		*/
+
+		// Display Priority in a TextView
+		final TextView descriptionView = (TextView)itemLayout.getChildAt(4);
+		descriptionView.setText(recipeItem.getDescription().toString());
+
+		// Display Time and Date.
+		final TextView dateView = (TextView)itemLayout.getChildAt(6);
+		dateView.setText(RecipeItem.FORMAT.format(recipeItem.getDate()));
+
+		// Return the View you just created
+		return itemLayout;
+
+	}
+}
